78fa00cb1e48b24018a12c1757de15c2f8be0fd7
Matthew Lyles <matlyles@gmail.com>
2020-04-01 16:48:14 -0700

    changes to the alertControllr

--- TodosCoreData/ViewController.swift
+++ TodosCoreData/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    let itemArray = ["Buy Milk", "Go to the doctors", "work on latest dev project"]
+    var itemArray = ["Buy Milk", "Go to the doctors", "work on latest dev project"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +24,23 @@ class ViewController: UITableViewController {
     
     
     @objc func handleAddGroup() {
-        print("handle adding a group")
+        let alert = UIAlertController(title: "Add New", message: "Create a todo group", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { [unowned self] (action) in
+            guard let textFieldData = alert.textFields?.first,
+                let readyText = textFieldData.text else { return }
+            print(readyText)
+            self.itemArray.append(readyText)
+            self.tableView.reloadData()
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+            self.dismiss(animated: true, completion: nil)
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "enter a new task..."
+        }
+        alert.addAction(cancel)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
